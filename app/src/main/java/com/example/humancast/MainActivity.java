16b1de70b39c5e922d7bcd0bc259af03f8628e3e
ngp6658 <ngp6658@rit.edu>
2021-05-25 18:09:23 -0400

    Initial commit!

---
+++ app/src/main/java/com/example/humancast/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.humancast;
+
+import android.content.Intent;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
